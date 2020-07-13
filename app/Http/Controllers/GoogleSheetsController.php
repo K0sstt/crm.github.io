fb6766c6edd737f188e68e3655987abcdb167ab2
Sergio <babik.sergey@gmail.com>
2020-07-13 19:53:05 +0300

    feature: create controller

---
+++ app/Http/Controllers/GoogleSheetsController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Entities\Row;
+use App\Services\GoogleSheetsService as Service;
+use Sheets;
+use Google_Client;
+use Google_Service_Drive;
+use Google_Service_Sheets;
+use Illuminate\Http\Request;
+
+class GoogleSheetsController extends Controller
+{
+    /**
+     * @var Service
+     */
+    private $service;
+
+    /**
+     * SheetsService constructor.
+     */
+    public function __construct(
+        Service $service
+    ) {
+        $this->service = $service;
+    }
+
+    /**
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function getAllRows()
+    {
+        $rows = $this->service->getAllRows();
+        new Row('1', '2');
+        return view('layouts.app', compact('rows'));
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function addRow(Request $request)
+    {
+        $row = new Row(
+            $request->first,
+            $request->second
+        );
+
+        $this->service->addRow($row);
+
+        return redirect()->route('home');
+    }
+}
